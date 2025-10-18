771e63a8848bcb35ff29cde113198076d2160c62
Dakota <tech33@gmx.ch>
2025-10-18 12:11:17 +0200

    add flashger tool page

--- include/EFConfig.h
+++ include/EFConfig.h
@@ -5,4 +5,4 @@
 #define HasDisplay   1      // or comment out to disable
 #define BOARD_NAME   "EF28"
 #define EF_USER_NAME ""
-
+//#define Mel 1 // or comment out to disable if you dont know mel4anie XD
